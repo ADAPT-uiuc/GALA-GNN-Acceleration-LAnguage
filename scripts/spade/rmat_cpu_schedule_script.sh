a344711a7ff1c52a229ff7dcd70a467cbdf38d23
damitha <damitha2@illinois.edu>
2023-06-12 18:27:46 -0500

    Update echo with printf and reduce the number of iters

--- scripts/spade/rmat_cpu_schedule_script.sh
+++ scripts/spade/rmat_cpu_schedule_script.sh
@@ -1,7 +1,6 @@
 # Name of the matrix to be downloaded from suite sparse
 data_path="/home/damitha2/GNN-Acceleration-Language/data_schedule/"
 test_path="../../build/tests/spade_cpu_spmm_impl"
-iters=20
 
 # Embedding configurations
 declare -a emb_Array=(32 \
@@ -51,42 +50,89 @@ echo "path,emb_size,col_tile,row_tile,loop_order,slice_size,barrier,work_div,reo
 for lo in "${loop_order_Array[@]}"; do
   (cd ../../build && rm -r * && cmake -DL_ORDER=$lo .. && make)
 
+  iters=200
   # Select the Graph + embedding
   for d in "$data_path"*/ ; do
     for emb_size in "${emb_Array[@]}"; do
       folder_name=$(basename $d)
       IFS='_' read -ra name_Array <<< "$folder_name"
 
+      if [[ $((name_Array[0])) -le 250000 ]]
+      then
       # Tiling
-      for col_tile in "${col_tile_Array[@]}"; do
-        col_chk=$((4*$((name_Array[0]))))
-        if [[ $col_tile -lt $col_chk ]]
-        then
-          for row_tile in "${row_tile_Array[@]}"; do
+        for col_tile in "${col_tile_Array[@]}"; do
+          col_chk=$((4*$((name_Array[0]))))
+          if [[ $col_tile -lt $col_chk ]]
+          then
+            for row_tile in "${row_tile_Array[@]}"; do
 
-            # If loop order 2, then try the other optimizations. Else just slicing
-            if [[ $lo == "2" ]]
-            then
+              # If loop order 2, then try the other optimizations. Else just slicing
+              if [[ $lo == "2" ]]
+              then
 
-              for reord in "${reord_Array[@]}"; do
-                printf '%s' "$d,$emb_size,$col_tile,$row_tile,$lo,no_slice,no_barr,no_wdiv,$reord,no_pref," >> transf_times
-                numactl --physcpubind=0-55 --interleave=all $test_path $d $emb_size $col_tile $row_tile 0 0 0 $reord 0 $iters >> transf_times
-              done
+                for reord in "${reord_Array[@]}"; do
+                  printf '%s' "$d,$emb_size,$col_tile,$row_tile,$lo,no_slice,no_barr,no_wdiv,$reord,no_pref," >> transf_times
+                  numactl --physcpubind=0-55 --interleave=all $test_path $d $emb_size $col_tile $row_tile 0 0 0 $reord 0 $iters >> transf_times
+                done
 
-            else
-              for slice in "${slice_Array[@]}"; do
-                if [[ $slice -lt $emb_size ]]
-                then
-                  for reord in "${reord_Array[@]}"; do
-                    echo "$d,$emb_size,$col_tile,$row_tile,$lo,$slice,no_barr,no_wdiv,$reord,no_pref," >> transf_times
-                    numactl --physcpubind=0-55 --interleave=all $test_path $d $emb_size $col_tile $row_tile $slice 0 0 $reord 0 $iters >> transf_times
-                  done
-                fi
-              done
-            fi
-          done
-        fi
-      done
+              else
+                for slice in "${slice_Array[@]}"; do
+                  if [[ $slice -lt $emb_size ]]
+                  then
+                    for reord in "${reord_Array[@]}"; do
+                      echo "$d,$emb_size,$col_tile,$row_tile,$lo,$slice,no_barr,no_wdiv,$reord,no_pref," >> transf_times
+                      numactl --physcpubind=0-55 --interleave=all $test_path $d $emb_size $col_tile $row_tile $slice 0 0 $reord 0 $iters >> transf_times
+                    done
+                  fi
+                done
+              fi
+            done
+          fi
+        done
+      fi
+    done
+  done
+
+  iters=20
+  # Select the Graph + embedding
+  for d in "$data_path"*/ ; do
+    for emb_size in "${emb_Array[@]}"; do
+      folder_name=$(basename $d)
+      IFS='_' read -ra name_Array <<< "$folder_name"
+
+      if [[ $((name_Array[0])) -gt 250000 ]]
+      then
+      # Tiling
+        for col_tile in "${col_tile_Array[@]}"; do
+          col_chk=$((4*$((name_Array[0]))))
+          if [[ $col_tile -lt $col_chk ]]
+          then
+            for row_tile in "${row_tile_Array[@]}"; do
+
+              # If loop order 2, then try the other optimizations. Else just slicing
+              if [[ $lo == "2" ]]
+              then
+
+                for reord in "${reord_Array[@]}"; do
+                  printf '%s' "$d,$emb_size,$col_tile,$row_tile,$lo,no_slice,no_barr,no_wdiv,$reord,no_pref," >> transf_times
+                  numactl --physcpubind=0-55 --interleave=all $test_path $d $emb_size $col_tile $row_tile 0 0 0 $reord 0 $iters >> transf_times
+                done
+
+              else
+                for slice in "${slice_Array[@]}"; do
+                  if [[ $slice -lt $emb_size ]]
+                  then
+                    for reord in "${reord_Array[@]}"; do
+                      echo "$d,$emb_size,$col_tile,$row_tile,$lo,$slice,no_barr,no_wdiv,$reord,no_pref," >> transf_times
+                      numactl --physcpubind=0-55 --interleave=all $test_path $d $emb_size $col_tile $row_tile $slice 0 0 $reord 0 $iters >> transf_times
+                    done
+                  fi
+                done
+              fi
+            done
+          fi
+        done
+      fi
     done
   done
 done
