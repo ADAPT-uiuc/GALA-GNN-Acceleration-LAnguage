592890efcf73a4949d07d5408468b885c542f5ab
damitha <damitha2@illinois.edu>
2023-06-26 16:29:30 -0500

    Add more tests

--- scripts/spade/test_normal.sh
+++ scripts/spade/test_normal.sh
@@ -11,3 +11,7 @@
 export OMP_NUM_THREADS=63
 
 numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl "/home/damitha2/projects/SparseAcc/data_exp_npy/mycielskian17/" 256 1024 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl "/home/damitha2/projects/SparseAcc/data_exp_npy/as-Skitter/" 256 1024 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl "/home/damitha2/projects/SparseAcc/data_exp_npy/asia_osm/" 256 1024 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl "/home/damitha2/projects/SparseAcc/data_exp_npy/delaunay_n21/" 256 1024 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl "/home/damitha2/projects/SparseAcc/data_exp_npy/com-LiveJournal/" 256 1024 2 32 0 0 0 0 10
