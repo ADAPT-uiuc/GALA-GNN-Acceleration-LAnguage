0032ed264841a60bcdc8eac524ccd8b42f5d7f83
damitha <damitha2@illinois.edu>
2023-06-26 14:59:27 -0500

    Add sbatch scripts to run the tests on iccp

---
+++ scripts/spade/test_barrier.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=21-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --mem-per-cpu=3800
+#SBATCH --job-name="spmm_bless"
+#SBATCH --partition=charithm
+#SBATCH --exclusive
+#SBATCH --output=out_bless.txt
+
+export OMP_NUM_THREADS=63
+
+numactl --interleave=all --physcpubind=0-63 tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/mycielskian17/" 256 1024 2 32 0 0 0 0 10
