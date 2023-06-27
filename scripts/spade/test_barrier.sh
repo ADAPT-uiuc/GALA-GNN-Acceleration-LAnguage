f4251051edf6ffa6c09130297533682df4da8ef3
damitha <damitha2@illinois.edu>
2023-06-27 11:18:42 -0500

    Adjust tests (80K tile -> 32K)

--- scripts/spade/test_barrier.sh
+++ scripts/spade/test_barrier.sh
@@ -9,9 +9,11 @@
 #SBATCH --output=out_bless.txt
 
 export OMP_NUM_THREADS=64
+emb_size=128
+col_tile_size=32000
 
-numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/mycielskian17/" 128 80000 2 32 0 0 0 0 10
-numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/as-Skitter/" 128 80000 2 32 0 0 0 0 10
-numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/asia_osm/" 128 80000 2 32 0 0 0 0 10
-numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/delaunay_n21/" 128 80000 2 32 0 0 0 0 10
-numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/com-LiveJournal/" 128 80000 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/mycielskian17/" $emb_size $col_tile_size 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/as-Skitter/" $emb_size $col_tile_size 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/asia_osm/" $emb_size $col_tile_size 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/delaunay_n21/" $emb_size $col_tile_size 2 32 0 0 0 0 10
+numactl --interleave=all --physcpubind=0-63 ../../build/tests/spade_cpu_spmm_impl_test_barrierless "/home/damitha2/projects/SparseAcc/data_exp_npy/com-LiveJournal/" $emb_size $col_tile_size 2 32 0 0 0 0 10
