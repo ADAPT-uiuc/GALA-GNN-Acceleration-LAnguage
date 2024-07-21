aaaa2d2b132df5086ca0b89cd483cb8caafecc52
damitha <damitha2@illinois.edu>
2024-07-20 19:12:42 -0500

    Local changes

--- tests/torch_cpp/gcn_model_col_tile.cu
+++ tests/torch_cpp/gcn_model_col_tile.cu
@@ -262,7 +262,7 @@ int main(int argc, char **argv) {
     out_emb.set_all(0);
     out_emb2.set_all(0);
 
-    // gSpMM(&adj, &input_emb, &out_emb2, wsum_aggr);
+    gSpMM(&adj, &input_emb, &out_emb2, wsum_aggr);
 
     std::cout << adj.nrows() << " " << adj.ncols() << " " << adj.nvals() << std::endl;
 
@@ -340,6 +340,16 @@ int main(int argc, char **argv) {
 
         if (epoch >= skip_cache_warmup) {
             times_arr.push_back(end - start);
+        }
+
+        for (int x = 0; x < nrows; x++){
+            for (int y = 0; y < emb_size; y++){
+                if (prediction[x][y].item<val_t>()!= out_emb2.vals_ptr()[x * emb_size + y]) {
+                    std::cout << "The results don't match at: " << x << "," << y << ":  " << prediction[x][y].item<val_t>() << ", "
+                              << out_emb2.vals_ptr()[x * emb_size + y] << std::endl;
+                    break;
+                }
+            }
         }
 
     }
