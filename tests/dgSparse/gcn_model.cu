bc4e778ed03bef970fedca081917b6f2eb5e0ca7
damitha <damitha2@illinois.edu>
2024-08-03 18:11:11 -0500

    Change the dgSpmm code to work with gala

--- tests/dgSparse/gcn_model.cu
+++ tests/dgSparse/gcn_model.cu
@@ -3,6 +3,7 @@
 //
 // Define a new Module.
 #include <cuda_runtime_api.h> // cudaMalloc, cudaMemcpy, etc.
+#include <cusparse.h>         // cusparseSpMM (>= v11.0) or cusparseScsrmm
 
 #include <torch/script.h>
 #include <iostream>
@@ -175,45 +176,6 @@ __global__ void csrspmm_non_transpose_parreduce_rowbalance_kernel(
     }
 }
 
-void csrspmm_non_transpose_parreduce_rowbalance(const SpMatCsrDescr_t spmatA,
-                                                const float *B, const int N,
-                                                float *C) {
-    // factor of thread coarsening
-    int coarsen_factor = (N >= 128) ? 4 : (N >= 64) ? 2 : 1;
-    // number of parallel warps along M-dimension
-    int Mdim_worker = spmatA.nrow;
-    // partition large-N and map to blockdim.y to help cache performance
-    int Ndim_threadblock = CEIL(N, coarsen_factor);
-
-    int ref_warp_per_tb = RefThreadPerBlock / 32;
-    int Mdim_warp_per_tb = ref_warp_per_tb;
-
-    // total number of warps
-    int gridDimX = CEIL(Mdim_worker, Mdim_warp_per_tb);
-    int gridDimY = Ndim_threadblock;
-    dim3 gridDim(gridDimX, gridDimY, 1);
-    dim3 blockDim(32, Mdim_warp_per_tb, 1);
-
-    if (coarsen_factor == 4) {
-        csrspmm_non_transpose_parreduce_rowbalance_kernel<4>
-        <<<gridDim, blockDim>>>(spmatA.nrow, N, spmatA.ncol, spmatA.indptr,
-                                spmatA.indices, spmatA.data, B, C);
-    } else if (coarsen_factor == 2) {
-        csrspmm_non_transpose_parreduce_rowbalance_kernel<2>
-        <<<gridDim, blockDim>>>(spmatA.nrow, N, spmatA.ncol, spmatA.indptr,
-                                spmatA.indices, spmatA.data, B, C);
-    } else {
-        csrspmm_non_transpose_parreduce_rowbalance_kernel<1>
-        <<<gridDim, blockDim>>>(spmatA.nrow, N, spmatA.ncol, spmatA.indptr,
-                                spmatA.indices, spmatA.data, B, C);
-    }
-}
-
-
-
-
-
-
 std::vector <at::Tensor> gather_forward_gcn(
         torch::Tensor input_dense,
         torch::Tensor offset_graph,
@@ -240,47 +202,35 @@ std::vector <at::Tensor> gather_forward_gcn(
     float alpha = 1.0f;
     float beta = 1.0f;
 
-    // Create the sparse / dense objects
-    cusparseHandle_t handle = NULL;
-    cusparseSpMatDescr_t matA;
-    cusparseDnMatDescr_t matB, matC;
-    void *dBuffer = NULL;
-    size_t bufferSize = 0;
+    // factor of thread coarsening
+    int coarsen_factor = (dcols >= 128) ? 4 : (dcols >= 64) ? 2 : 1;
+    // number of parallel warps along M-dimension
+    int Mdim_worker = nrows;
+    // partition large-N and map to blockdim.y to help cache performance
+    int Ndim_threadblock = CEIL(dcols, coarsen_factor);
 
-    CUSPARSE_CHECK(cusparseCreate(&handle));
-    CUSPARSE_CHECK(cusparseCreateCsr(&matA, nrows, nrows, nvals,
-                                     offset_ptr, col_ptr, val_ptr,
-                                     CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I, // Need to change these
-                                     CUSPARSE_INDEX_BASE_ZERO, CUDA_R_32F));
-    // Create dense matrix B
-    CUSPARSE_CHECK(cusparseCreateDnMat(&matB, nrows, dcols, dcols, iden_ptr,
-                                       CUDA_R_32F, CUSPARSE_ORDER_ROW)); // changed
-    // Create dense matrix C
-    CUSPARSE_CHECK(cusparseCreateDnMat(&matC, nrows, dcols, dcols, oden_array,
-                                       CUDA_R_32F, CUSPARSE_ORDER_ROW)); // changed
+    int ref_warp_per_tb = RefThreadPerBlock / 32;
+    int Mdim_warp_per_tb = ref_warp_per_tb;
 
-    // allocate an external buffer if needed
-    CUSPARSE_CHECK(cusparseSpMM_bufferSize(
-            handle,
-            CUSPARSE_OPERATION_NON_TRANSPOSE,
-            CUSPARSE_OPERATION_NON_TRANSPOSE,
-            &alpha, matA, matB, &beta, matC, CUDA_R_32F,
-            CUSPARSE_SPMM_CSR_ALG2,
-            &bufferSize));
-    CUDA_CHECK(cudaMalloc(&dBuffer, bufferSize));
+    // total number of warps
+    int gridDimX = CEIL(Mdim_worker, Mdim_warp_per_tb);
+    int gridDimY = Ndim_threadblock;
+    dim3 gridDim(gridDimX, gridDimY, 1);
+    dim3 blockDim(32, Mdim_warp_per_tb, 1);
 
-    CUSPARSE_CHECK(cusparseSpMM(handle,
-                                CUSPARSE_OPERATION_NON_TRANSPOSE,
-                                CUSPARSE_OPERATION_NON_TRANSPOSE,
-                                &alpha, matA, matB, &beta, matC, CUDA_R_32F,
-                                CUSPARSE_SPMM_CSR_ALG2,
-                                dBuffer));
-
-    CUSPARSE_CHECK(cusparseDestroySpMat(matA));
-    CUSPARSE_CHECK(cusparseDestroyDnMat(matB));
-    CUSPARSE_CHECK(cusparseDestroyDnMat(matC));
-    CUSPARSE_CHECK(cusparseDestroy(handle));
-    CUDA_CHECK(cudaFree(dBuffer));
+    if (coarsen_factor == 4) {
+        csrspmm_non_transpose_parreduce_rowbalance_kernel<4>
+        <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
+                                col_ptr, val_ptr, iden_ptr, oden_array);
+    } else if (coarsen_factor == 2) {
+        csrspmm_non_transpose_parreduce_rowbalance_kernel<2>
+        <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
+                                col_ptr, val_ptr, iden_ptr, oden_array);
+    } else {
+        csrspmm_non_transpose_parreduce_rowbalance_kernel<1>
+        <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
+                                col_ptr, val_ptr, iden_ptr, oden_array);
+    }
 
     return {output_dense};
 }
@@ -305,6 +255,148 @@ struct GCN : torch::nn::Module {
     // Use one of many "standard library" modules.
     torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
 };
+
+//int main(int argc, const char **argv) {
+//    /// check command-line argument
+//
+//    if (argc < 2) {
+//        printf("Require command-line argument: name of the sparse matrix file in "
+//               ".mtx format.\n");
+//        return EXIT_FAILURE;
+//    }
+//
+//    //
+//    // Load sparse matrix
+//    //
+//
+//    int M;                               // number of A-rows
+//    int K;                               // number of A-columns
+//    int nnz;                             // number of non-zeros in A
+//    std::vector<int> csr_indptr_buffer;  // buffer for indptr array in CSR format
+//    std::vector<int> csr_indices_buffer; // buffer for indices (column-ids) array
+//    // in CSR format
+//    // load sparse matrix from mtx file
+//    read_mtx_file(argv[1], M, K, nnz, csr_indptr_buffer, csr_indices_buffer);
+//    printf("Finish reading matrix %d rows, %d columns, %d nnz. \nIgnore original "
+//           "values and use randomly generated values.\n",
+//           M, K, nnz);
+//
+//    // Create GPU arrays
+//    int N = 128; // number of B-columns
+//    if (argc > 2) {
+//        N = atoi(argv[2]);
+//    }
+//    assert(
+//            N > 0 &&
+//            "second command-line argument is number of B columns, should be >0.\n");
+//
+//    float *B_h = NULL, *C_h = NULL, *csr_values_h = NULL, *C_ref = NULL;
+//    float *B_d = NULL, *C_d = NULL, *csr_values_d = NULL;
+//    int *csr_indptr_d = NULL, *csr_indices_d = NULL;
+//
+//    B_h = (float *)malloc(sizeof(float) * K * N);
+//    C_h = (float *)malloc(sizeof(float) * M * N);
+//    C_ref = (float *)malloc(sizeof(float) * M * N);
+//    csr_values_h = (float *)malloc(sizeof(float) * nnz);
+//    if (!B_h || !C_h || !C_ref || !csr_values_h) {
+//        printf("Host allocation failed.\n");
+//        return EXIT_FAILURE;
+//    }
+//
+//    fill_random(csr_values_h, nnz);
+//    fill_random(B_h, K * N);
+//
+//    CUDA_CHECK(cudaMalloc((void **)&B_d, sizeof(float) * K * N));
+//    CUDA_CHECK(cudaMalloc((void **)&C_d, sizeof(float) * M * N));
+//    CUDA_CHECK(cudaMalloc((void **)&csr_values_d, sizeof(float) * nnz));
+//    CUDA_CHECK(cudaMalloc((void **)&csr_indptr_d, sizeof(int) * (M + 1)));
+//    CUDA_CHECK(cudaMalloc((void **)&csr_indices_d, sizeof(int) * nnz));
+//
+//    CUDA_CHECK(
+//            cudaMemcpy(B_d, B_h, sizeof(float) * K * N, cudaMemcpyHostToDevice));
+//    CUDA_CHECK(cudaMemset(C_d, 0x0, sizeof(float) * M * N));
+//    CUDA_CHECK(cudaMemcpy(csr_values_d, csr_values_h, sizeof(float) * nnz,
+//                          cudaMemcpyHostToDevice));
+//    CUDA_CHECK(cudaMemcpy(csr_indptr_d, csr_indptr_buffer.data(),
+//                          sizeof(int) * (M + 1), cudaMemcpyHostToDevice));
+//    CUDA_CHECK(cudaMemcpy(csr_indices_d, csr_indices_buffer.data(),
+//                          sizeof(int) * nnz, cudaMemcpyHostToDevice));
+//
+//    SpMatCsrDescr_t spmatA{M, K, nnz, csr_indptr_d, csr_indices_d, csr_values_d};
+//    gespmmAlg_t algs[] = {
+//            GESPMM_ALG_SEQREDUCE_ROWBALANCE,  GESPMM_ALG_PARREDUCE_ROWBALANCE,
+//            GESPMM_ALG_SEQREDUCE_NNZBALANCE,  GESPMM_ALG_PARREDUCE_NNZBALANCE,
+//            GESPMM_ALG_ROWCACHING_ROWBALANCE, GESPMM_ALG_ROWCACHING_NNZBALANCE};
+//
+//    for (auto alg : algs) {
+//        //
+//        // Run GE-SpMM and check result
+//        //
+//
+//        CUDA_CHECK(cudaMemset(C_d, 0x0, sizeof(float) * M * N));
+//
+//        gespmmCsrSpMM(spmatA, B_d, N, C_d, true, alg);
+//
+//        cudaDeviceSynchronize();
+//        CUDA_CHECK(
+//                cudaMemcpy(C_h, C_d, sizeof(float) * M * N, cudaMemcpyDeviceToHost));
+//
+//        spmm_reference_host<int, float>(M, N, K, csr_indptr_buffer.data(),
+//                                        csr_indices_buffer.data(), csr_values_h,
+//                                        B_h, C_ref);
+//
+//        // benchmark GE-SpMM performance
+//
+//        GpuTimer gpu_timer;
+//        int warmup_iter = 10;
+//        int repeat_iter = 100;
+//        for (int iter = 0; iter < warmup_iter + repeat_iter; iter++) {
+//            if (iter == warmup_iter) {
+//                gpu_timer.start();
+//            }
+//
+//            gespmmCsrSpMM(spmatA, B_d, N, C_d, true, alg);
+//        }
+//        gpu_timer.stop();
+//
+//        float kernel_dur_msecs = gpu_timer.elapsed_msecs() / repeat_iter;
+//
+//        float MFlop_count = (float)nnz / 1e6 * N * 2;
+//
+//        float gflops = MFlop_count / kernel_dur_msecs;
+//
+//        printf("[GE-SpMM][Alg: %d] Report: spmm A(%d x %d) * B(%d x %d) sparsity "
+//               "%f (nnz=%d) \n Time %f (ms), Throughput %f (gflops).\n",
+//               alg, M, K, K, N, (float)nnz / M / K, nnz, kernel_dur_msecs,
+//               gflops);
+//    }
+//
+//    /// free memory
+//
+//    if (B_h)
+//        free(B_h);
+//    if (C_h)
+//        free(C_h);
+//    if (C_ref)
+//        free(C_ref);
+//    if (csr_values_h)
+//        free(csr_values_h);
+//    if (B_d)
+//        CUDA_CHECK(cudaFree(B_d));
+//    if (C_d)
+//        CUDA_CHECK(cudaFree(C_d));
+//    if (csr_values_d)
+//        CUDA_CHECK(cudaFree(csr_values_d));
+//    if (csr_indptr_d)
+//        CUDA_CHECK(cudaFree(csr_indptr_d));
+//    if (csr_indices_d)
+//        CUDA_CHECK(cudaFree(csr_indices_d));
+//    if (workspace)
+//        CUDA_CHECK(cudaFree(workspace));
+//
+//    return EXIT_SUCCESS;
+//}
+
 
 int main(int argc, char **argv) {
     typedef typename SM::itype iT;
