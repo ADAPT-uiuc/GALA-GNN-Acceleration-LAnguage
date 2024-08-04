a004ab35a5c4901cd08887ee6e92f3f7c49b9f8f
damitha <damitha2@illinois.edu>
2024-08-03 19:02:14 -0500

    Add transpose mtx

--- tests/dgSparse/gcn_model.cu
+++ tests/dgSparse/gcn_model.cu
@@ -65,6 +65,116 @@ struct SpMatCsrDescr_t {
 };
 
 template <int CoarsenFactor>
+__global__ void csrspmm_rowcaching_rowbalance_kernel(
+        const int M, const int N, const int K, const int csr_indptr[],
+        const int csr_indices[], const float csr_data[], const float B[],
+        float C[]) {
+    int warp_id = threadIdx.x >> 5;
+    int lane_id = threadIdx.x & 31;
+
+    extern __shared__ int shared_mem[];
+    int *workspace_indices = &shared_mem[(warp_id << 5)];
+    float *workspace_data =
+            (float *)(workspace_indices +
+                      blockDim.x); // float and int has the same size
+
+    // get the sparse-value range of this row
+    int row_id = blockIdx.x * (blockDim.x >> 5) + warp_id;
+    if (row_id >= M)
+        return;
+    int start = csr_indptr[row_id];
+    int end = csr_indptr[row_id + 1];
+
+    // get the dense column offset
+    int col_offset = blockIdx.y * 32 * CoarsenFactor;
+    const float *B_lanes[CoarsenFactor];
+#pragma unroll
+    for (int i = 0; i < CoarsenFactor; i++) {
+        B_lanes[i] = B + col_offset + lane_id + i * 32;
+    }
+    int ldB = N;
+
+    // declare accumulators
+    float c[CoarsenFactor] = {0.0f};
+    int ldC = N;
+
+    // N-dimension residual handling
+    if (blockIdx.y == gridDim.y - 1)
+        goto Ndim_Residue;
+
+    // iterate over the sparse row
+    for (int p = start; p < end; p += 32) {
+        // copy a bucket of sparse row elements into shared memory
+        if (p + lane_id < end) {
+            workspace_data[lane_id] =
+                    __guard_load_default_one<float>(csr_data, (p + lane_id));
+            workspace_indices[lane_id] = csr_indices[p + lane_id];
+        } else {
+            workspace_data[lane_id] = 0.0f;
+            workspace_indices[lane_id] = 0;
+        }
+        __syncwarp();
+// do MAC computation using buffered elements
+#pragma unroll
+        for (int pp = 0; pp < 32; pp++) {
+            int k = workspace_indices[pp];
+            float v = workspace_data[pp];
+#pragma unroll
+            for (int i = 0; i < CoarsenFactor; i++) {
+                c[i] += v * B_lanes[i][k * ldB];
+            }
+        }
+    }
+
+// write results
+#pragma unroll
+    for (int i = 0; i < CoarsenFactor; i++) {
+        float *C_lane = B_lanes[i] - B + (C + row_id * ldC);
+        *C_lane = c[i];
+    }
+    return;
+
+    Ndim_Residue:
+    int valid_lane_num = CEIL(N - col_offset - lane_id, 32);
+
+    // iterate over the sparse row
+    for (int p = start; p < end; p += 32) {
+        // copy a bucket of sparse row elements into shared memory
+        if (p + lane_id < end) {
+            workspace_data[lane_id] =
+                    __guard_load_default_one<float>(csr_data, (p + lane_id));
+            workspace_indices[lane_id] = csr_indices[p + lane_id];
+        } else {
+            workspace_data[lane_id] = 0.0f;
+            workspace_indices[lane_id] = 0;
+        }
+        __syncwarp();
+// do MAC computation using buffered elements
+#pragma unroll
+        for (int pp = 0; pp < 32; pp++) {
+            int k = workspace_indices[pp];
+            float v = workspace_data[pp];
+#pragma unroll
+            for (int i = 0; i < CoarsenFactor; i++) {
+                if (i < valid_lane_num) {
+                    c[i] += v * B_lanes[i][k * ldB];
+                }
+            }
+        }
+    }
+
+// write results
+#pragma unroll
+    for (int i = 0; i < CoarsenFactor; i++) {
+        float *C_lane = B_lanes[i] - B + (C + row_id * ldC);
+        if (i < valid_lane_num) {
+            *C_lane = c[i];
+        }
+    }
+    return;
+}
+
+template <int CoarsenFactor>
 __global__ void csrspmm_non_transpose_parreduce_rowbalance_kernel(
         const int M, const int N, const int K, const int csr_indptr[],
         const int csr_indices[], const float csr_data[], const float B[],
@@ -219,17 +329,26 @@ std::vector <at::Tensor> gather_forward_gcn(
     dim3 blockDim(32, Mdim_warp_per_tb, 1);
 
     if (coarsen_factor == 4) {
-        csrspmm_non_transpose_parreduce_rowbalance_kernel<4>
+        csrspmm_rowcaching_rowbalance_kernel<4>
         <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
                                 col_ptr, val_ptr, iden_ptr, oden_array);
+//        csrspmm_non_transpose_parreduce_rowbalance_kernel<4>
+//        <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
+//                                col_ptr, val_ptr, iden_ptr, oden_array);
     } else if (coarsen_factor == 2) {
-        csrspmm_non_transpose_parreduce_rowbalance_kernel<2>
+        csrspmm_rowcaching_rowbalance_kernel<2>
         <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
                                 col_ptr, val_ptr, iden_ptr, oden_array);
+//        csrspmm_non_transpose_parreduce_rowbalance_kernel<2>
+//        <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
+//                                col_ptr, val_ptr, iden_ptr, oden_array);
     } else {
-        csrspmm_non_transpose_parreduce_rowbalance_kernel<1>
+        csrspmm_rowcaching_rowbalance_kernel<1>
         <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
                                 col_ptr, val_ptr, iden_ptr, oden_array);
+//        csrspmm_non_transpose_parreduce_rowbalance_kernel<1>
+//        <<<gridDim, blockDim>>>(nrows, dcols, nrows, offset_ptr,
+//                                col_ptr, val_ptr, iden_ptr, oden_array);
     }
 
     return {output_dense};
