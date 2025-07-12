0d38d256212b7c467473d838df033c5972114a43
damitha <damitha2@illinois.edu>
2025-07-11 19:51:04 -0500

    Debug setup

--- scripts/e2e/f19.sh
+++ scripts/e2e/f19.sh
@@ -3,7 +3,7 @@
 source ~/miniforge3/bin/activate gala
 cd ../Evaluations
 python Figure-19.py
-source ../WiseGraph/h100/cxgnn/bin/activate
+source ../Environments/WiseGraph/h100/cxgnn/bin/activate
 python WiseGraph.py --job F18n19 --h100
 source ~/miniforge3/bin/activate gala
 python Figure-19.py --job stat
