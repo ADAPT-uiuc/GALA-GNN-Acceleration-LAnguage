841007496cb33c4ca2ab2d94787375d6d3cced32
Alexander Bakst <abakst@aarno-labs.com>
2026-05-22 13:11:00 -0700

    generate test data

---
+++ tests/gala_test.cpp
@@ -0,0 +1,23 @@
+//
+#include "gala_driver.h"
+
+std::vector<CIRNode*> GALAFEContext::program;
+std::vector<RelationEdge*> GALAFEContext::dependencies;
+std::vector<RelationEdge*> GALAFEContext::associations;
+std::vector<TransformEdge*> GALAFEContext::transforms;
+
+bool GALAFEContext::operator_reordering = true;
+bool GALAFEContext::sparse_rewrites = true;
+bool GALAFEContext::train_code_motion = true;
+bool GALAFEContext::training_subgraph = true;
+bool GALAFEContext::print_accuracy = true;
+bool GALAFEContext::print_memory = false;
+bool GALAFEContext::use_long = false;
+
+std::string GALAFEContext::opt_input = "";
+
+ModelConfig m1;
+
+int main(int argc, char **argv) {
+	return gala_run(argc, argv, true);
+}
